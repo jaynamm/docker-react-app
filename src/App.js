bb039eeb30b2e9fe091c883f7e9d81cac1208c3f
jaynamm <polky0421@gmail.com>
2021-04-14 15:27:53 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ! Go ! 고고 !
         </a>
       </header>
     </div>
